a3d4cf0b562583b242ab006a67dea83f5e419169
bvebars <bvebars@gmail.com>
2020-01-24 17:49:24 +0700

    Add: jwt token, hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom'
 import {useRoutes} from "./routes";
+import {useAuth} from "./hooks/auth.hook";
+import {AuthContext} from "./context/AuthContext";
 
 function App() {
-    const routes = useRoutes(false)
-  return (
-    <div >
-        <BrowserRouter>
-            {routes}
-        </BrowserRouter>
-    </div>
-  );
+    const {token, login, userId, logout} = useAuth();
+    const isAuthenticated = !!token;
+    const routes = useRoutes(isAuthenticated);
+    return (
+        <AuthContext.Provider value={{
+            token, login, userId, logout, isAuthenticated
+        }}>
+            <div>
+                <BrowserRouter>
+                    {routes}
+                </BrowserRouter>
+            </div>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,23 +1,57 @@
-import React from "react";
-import {Button, Checkbox, Container, Form} from "semantic-ui-react";
+import React, {useContext, useState} from "react";
+import {Button, Container, Form, Input} from "semantic-ui-react";
 import 'semantic-ui-css/semantic.min.css'
+import {useHttp} from "../hooks/http.hook";
+import {AuthContext} from "../context/AuthContext";
 
 export const AuthPage = () => {
+    const auth = useContext(AuthContext)
+    const {loading, error, request} = useHttp();
+    const [form, setForm] = useState({email: '', password: ''});
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    };
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/user/create', 'POST', {...form});
+            console.log('Data', data)
+        } catch (e) {}
+    };
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/user/login', 'POST', {...form});
+           auth.login(data.token, data.userId)
+        } catch (e) {}
+    };
+
+   let errors = {};
+    if(error) {
+        errors = {
+            error: {
+                content: error,
+                pointing: 'below',
+            }
+        };
+    }
+
     return (
         <div>
-
             <Container text>
                 <h1>Регистрация пользователя</h1>
                 <Form>
-                    <Form.Field>
-                        <label>Введите телефон</label>
-                        <input
-                            placeholder='+7'
-                            id="phone"
-                            type="text"
-                            name="phone"
-                        />
-                    </Form.Field>
+                    <Form.Field
+                        id='phone'
+                        control={Input}
+                        label='Телефон'
+                        placeholder='+7'
+                        type="text"
+                        name="phone"
+                        onChange={changeHandler}
+                        {...errors}
+                    />
                     <Form.Field>
                         <label>Введите пароль</label>
                         <input
@@ -25,9 +59,22 @@ export const AuthPage = () => {
                             id="password"
                             type="password"
                             name="password"
+                            onChange={changeHandler}
                         />
                     </Form.Field>
-                    <Button type='submit'>Отправить</Button>
+
+                    <Button
+                        type='submit'
+                        onClick={registerHandler}
+                        disabled={loading}
+                    >Регистрация
+                    </Button>
+                    <Button
+                        type='submit'
+                        onClick={loginHandler}
+                        disabled={loading}
+                    >Вход
+                    </Button>
                 </Form>
             </Container>
         </div>
